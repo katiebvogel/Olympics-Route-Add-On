a91ef9732e9503173a55f8af765ee9631622c197
Katie Vogel <katiebvogel@gmail.com>
2016-07-13 15:08:45 -0500

    basic functioning is all there

--- public/client.js
+++ public/client.js
@@ -1,94 +1,68 @@
 var app = angular.module('olympicApp', ['ngRoute']);
 
-// app.controller('OlympicController', function($scope, $http){
 
 
   angular.module('olympicApp').config(function($routeProvider, $locationProvider){
     $routeProvider
-      .when('/wrest', {
-        templateUrl: 'views/wrest.html',
+      .when('/wrestlers', {
+        templateUrl: 'views/wrestlers.html',
         controller: 'WrestController'
       })
-      .when('/lift', {
-        templateUrl: 'views/lift.html',
+      .when('/lifters', {
+        templateUrl: 'views/lifters.html',
         controller: 'LiftController'
       })
-      .when('/box', {
-        templateUrl: 'views/box.html',
+      .when('/boxers', {
+        templateUrl: 'views/boxers.html',
         controller: 'BoxController'
       })
-      .when('/kayak', {
-        templateUrl: 'views/kayak.html',
+      .when('/kayakers', {
+        templateUrl: 'views/kayakers.html',
         controller: 'KayakController'
       })
-      .when('/judo', {
-        templateUrl: 'views/judo.html',
+      .when('/judoka', {
+        templateUrl: 'views/judoka.html',
         controller: 'JudoController'
       })
 
     $locationProvider.html5Mode(true);
   })
 
-  angular.module('olympicApp').controller('WrestController', function($scope){
-    console.log(response);
-    $scope.wrest = ('Our wrestler is ' + response.data.name + ' from ' + response.data.location);
-    return $scope.wrest;
+  angular.module('olympicApp').controller('WrestController', function($scope, $http){
+  $http.get('olympics/wrestlers').then(function(response){
+  console.log(response);
+  $scope.wrestlers = ('Our wrestler is ' + response.data.name + ' from ' + response.data.location);
+  return $scope.wrestlers;
+  })
+});
+
+  angular.module('olympicApp').controller('LiftController', function($scope, $http){     $http.get('olympics/lifters').then(function(response){
+      console.log(response);
+      $scope.lifters = ('Our weightlifter is ' + response.data.name + ' from ' + response.data.location);
+      return $scope.lifters;
+      })
   });
 
-  angular.module('olympicApp').controller('LiftController', function($scope){
-    console.log(response);
-    $scope.lift = ('Our weightlifter is ' + response.data.name + ' from ' + response.data.location);
-    return $scope.lift;
-  });
+  angular.module('olympicApp').controller('BoxController', function($scope, $http){
+  $http.get('olympics/boxers').then(function(response){
+  console.log(response);
+  $scope.boxers = ('Our boxer is ' + response.data.name + ' from ' + response.data.location);
+  return $scope.boxers;
+  })
+});
 
-  angular.module('olympicApp').controller('BoxController', function($scope){
+  angular.module('olympicApp').controller('KayakController', function($scope, $http){
+  $http.get('olympics/kayakers').then(function(response){
+  console.log(response);
+  $scope.kayakers =  ('Our kayaker is ' + response.data.name + ' from ' + response.data.location);
+  return $scope.kayakers;
+  })
+});
 
-  });
-
-  angular.module('olympicApp').controller('KayakController', function($scope){
-
-  });
-  angular.module('olympicApp').controller('JudoController', function($scope){
-
-  });
-
-
-
-  $scope.getFive = function(){
-
-
-      $http.get('/olympics/lifters').then(function(response){
-      console.log(response);
-      $scope.lift = ('Our weightlifter is ' + response.data.name + ' from ' + response.data.location);
-      return $scope.lift;
-      })
-
-      $http.get('/olympics/wrestlers').then(function(response){
-      console.log(response);
-      $scope.wrest = ('Our wrestler is ' + response.data.name + ' from ' + response.data.location);
-      return $scope.wrest;
-      })
-
-      $http.get('/olympics/boxers').then(function(response){
-      console.log(response);
-      $scope.box = ('Our boxer is ' + response.data.name + ' from ' + response.data.location);
-      return $scope.box;
-      })
-
-      $http.get('/olympics/kayakers').then(function(response){
-      console.log(response);
-      $scope.kayak =  ('Our kayaker is ' + response.data.name + ' from ' + response.data.location);
-      return $scope.kayak;
-      })
-
-      $http.get('/olympics/judoka').then(function(response){
-      console.log(response);
-      $scope.judo = ('Our judoka is ' + response.data.name + ' from ' + response.data.location);
-      return $scope.judo;
-      })
-
-  }
-
-j
-
+  angular.module('olympicApp').controller('JudoController', function($scope, $http){
+  $http.get('olympics/judoka').then(function(response){
+  console.log(response);
+  $scope.judoka = ('Our judoka is ' + response.data.name + ' from ' + response.data.location);
+  return $scope.judoka;
+  })
 });
